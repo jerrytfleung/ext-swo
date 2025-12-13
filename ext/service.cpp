140eebb2f84c79c255038815909ade5587c12559
Jerry Leung <tingfung.leung@solarwinds.com>
2025-12-12 17:45:05 -0800

    Updated

--- ext/service.cpp
+++ ext/service.cpp
@@ -21,25 +21,29 @@ namespace Solarwinds {
         try {
             th_ = std::thread([&]() {
                 while (true) {
+                    php_printf("Service pid: %u Create lock\n", getpid());
                     std::unique_lock<std::mutex> lock(mutex_);
+                    php_printf("Service pid: %u conditional variable wait for\n", getpid());
                     if (cv_.wait_for(lock, interval_ * 1ms, [&]() { return stopping_ || !init_; })) {
                         if (stopping_) {
-                            php_printf("SettingService pid: %u Stopping\n", getpid());
+                            php_printf("Service pid: %u Stopping\n", getpid());
                             break;
                         }
                         if (!init_) {
                             init_ = true;
-                            php_printf("SettingService pid: %u Init task\n", getpid());
+                            php_printf("Service pid: %u Init task\n", getpid());
                             task();
+                            php_printf("Service pid: %u Init task completed\n", getpid());
                         }
                     } else {
-                        php_printf("SettingService pid: %u Time to do the task\n", getpid());
+                        php_printf("Service pid: %u Time to do the task\n", getpid());
                         task();
+                        php_printf("Service pid: %u ticker task completed\n", getpid());
                     }
                 }
             });
         } catch (const std::system_error& e) {
-            php_printf("SettingService pid: %u System error %s\n", getpid(), e.what());
+            php_printf("Service pid: %u System error %s\n", getpid(), e.what());
         }
     }
     void Service::stop() {

d5a8fe7d734d638c89eec90307e6127f73daecd1
Jerry Leung <tingfung.leung@solarwinds.com>
2026-01-14 10:14:12 -0800

    Updated

--- ext/setting_service_c_wrapper.cpp
+++ ext/setting_service_c_wrapper.cpp
@@ -1,9 +1,8 @@
 #include "setting_service_c_wrapper.h"
-// #include "setting_service.h"
+#include "setting_service.h"
 #include <cstring>
 
 void* Setting_Service_Allocate(char *collector, char *service_key) {
-    /*
     if (collector != nullptr && std::strlen(collector) > 0 && service_key != nullptr && std::strlen(service_key) > 0) {
         std::string collector_str = collector;
         std::string service_key_str = service_key;
@@ -12,23 +11,19 @@ void* Setting_Service_Allocate(char *collector, char *service_key) {
             return static_cast<void *>(p);
         }
     }
-    */
     return nullptr;
 }
 
 void Setting_Service_Free(void* service) {
-    /*
     if (service != nullptr) {
         auto s = static_cast<Solarwinds::SettingService *>(service);
         if (s != nullptr) {
             delete s;
         }
     }
-    */
 }
 
 bool Setting_Service_Get_Setting(void *service, char* ans) {
-    /*
     if (service != nullptr) {
         auto s = static_cast<Solarwinds::SettingService *>(service);
         if (s != nullptr) {
@@ -37,6 +32,5 @@ bool Setting_Service_Get_Setting(void *service, char* ans) {
             return true;
         }
     }
-    */
     return false;
 }
